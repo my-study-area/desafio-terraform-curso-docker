4d558cfe933ee01709ae219ab89f046f3520aaf6
Adriano Avelino <adriano-st777@hotmail.com>
2022-06-02 21:15:28 -0300

    separa o provider num arquivo separado

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.16.0"
-    }
-  }
-}
-
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
-}
-
 resource "docker_image" "nginx-curso-alura-docker" {
   name = var.image.nginx
 }
